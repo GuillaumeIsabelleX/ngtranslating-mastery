ddd3250bf6e545810d1ec55436207f772a56ba1f
Guillaume Isabelle <jgi@jgwill.com>
2019-03-22 13:57:31 -0400

    x

--- sample-angular-app/src/app/app.module.ts
+++ sample-angular-app/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+//@urir https://angular.io/guide/i18n#i18n-pipes
+//@stcgoal Angular pipes can help you with internationalization: the DatePipe, CurrencyPipe, DecimalPipe and PercentPipe use locale data to format data based on the LOCALE_ID.
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeFrExtra from '@angular/common/locales/extra/fr';
+
+registerLocaleData(localeFr, 'fr-FR', localeFrExtra);
+
+
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
