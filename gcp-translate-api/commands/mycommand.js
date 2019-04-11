b323d55be9d4915495075825f5ebcedaf883f8fb
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 14:46:11 -0400

    Migrating into a newstring command

--- gcp-translate-api/commands/mycommand.js
+++ gcp-translate-api/commands/mycommand.js
@@ -4,9 +4,11 @@ exports.default = {
     args: ['arg1', '?arg2'],
     options: [
         ['-f','--force', 'Override it!'],
-        ['--port','--port comport com4', 'Select your COMPort.']
+        ['--port','--port comport com4', 'Select your COMPort.'],
+        ['-t','--source','English text source to translate'],
+        ['-l','--lang','target language']
     ],
-    description: 'My awsome command',
+    description: 'My awsome command\n\tIt will greatly help creating command line.\n--------------------------------\n',
     action: (arg1, arg2, options) => {
         console.log("first arg : " + arg1);
         console.log("2 arg : " + arg2);
--- gcp-translate-api/commands/newstring.js
+++ gcp-translate-api/commands/newstring.js
@@ -1,22 +1,156 @@
+
+
+const path = require('path');
+const fs = require('fs');
+var nsset = require('nsset');
+var JsonFile = require('@exponent/json-file');
+const { Translate } = require('@google-cloud/translate');
+
+
+// Instantiates a client
+const translate = new Translate({
+});
+
+
 //@STCgoal Add a new string to the Translation
 //
 
 /* /commands/newstring.js */
 exports.default = {
-   cmd: __filename,
-   args: ['textSource','targetObject'],
-   options: [
-       ['-f','--force', 'Override it!'],
-       ['-t','--source','English text source to translate'],
-       ['-l','--lang','target language']
-   ],
-   description: 'Add a new string to all translation files.\n--------------------------------\n'
-   + '#> transapp newstring "hello world" "HOME.GREETING"'
-   ,
-   action: (arg1, arg2, options) => {
-       console.log("first arg : " + arg1);
-       console.log("2 arg : " + arg2);
-       console.log(options);
-       return 1;
-   }
+    cmd: __filename,
+    args: ['textSource', 'targetObject'],
+    options: [
+        ['-f', '--force', 'Override it!'],
+        ['-t', '--source', 'English text source to translate'],
+        ['-l', '--lang', 'target language']
+        ['-i', '--init', 'Init a new lang - reads all english string and create the new. use with --lang']
+    ],
+    description: 'Add a new string to all translation files.\n--------------------------------\n'
+        + '#> transapp newstring "hello world" "HOME.GREETING"'
+    ,
+    action: (txt, target, options) => {
+        console.log("New Text : " + txt);
+        console.log("Namespace : " + target);
+        console.log(options);
+
+
+        //Assuming you runs from the root of your app.
+
+        var i18nroot = "/src/assets/i18n";
+
+        //@a Reads all file in there
+
+
+        readTransation()
+            .then((files) => {
+                //listing all files using forEach
+                files.forEach(function (file) {
+                    // Do whatever you want to do with the file
+                    var filePath = i18nroot + '/' + file;
+                    var langCode = file.replace(".json", "");
+                    console.log(filePath);
+
+                    var fileTranslation = new JsonFile(filePath, { cantReadFileDefault: {} });
+
+
+                    //Read the translation File
+                    fileTranslation.readAsync()
+                        .then((obj) => {
+
+
+                            if (langCode != "en") {
+                                // Translates some text into Russian
+                                translate
+                                    .translate(txt, langCode)
+                                    .then(results => {
+                                        const translation = results[0];
+
+                                        //Adds a dummy value to test translation files
+                                        nsset.set(target, obj, translation);
+
+                                        //@STCgoal Writes the new JSON file
+                                        fileTranslation.writeAsync(obj);
+
+                                        console.log(`Text: ${txt}`);
+                                        console.log(`Translation: ${translation}`);
+                                    })
+                                    .catch(err => {
+                                        console.error('ERROR:', err);
+                                    });
+                            }
+                            else { //@status IS ENGLISH Original, not requiring translation
+
+                                //Adds a dummy value to test translation files
+                                nsset.set(target, obj, txt);
+
+                                //@STCgoal Writes the new JSON file
+                                fileTranslation.writeAsync(obj);
+                            }
+
+
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+
+
+                    // var obj = require(filePath);
+                    // // var obj = JSON.parse(t);
+                    // var keys = Object.keys(obj);
+                    // for (var i = 0; i < keys.length; i++) {
+                    //    var v = obj[keys[i]];
+                    //    console.log(v);
+                    // }
+                });
+                console.log(`
+               Angular HTML Use:
+               {{ '${target}' | translate }}
+               `);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+
+
+
+
+        return 1;
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Reads the directory
+ */
+function readTransation() {
+    return new Promise(function (resolving, rejecting) {
+
+        //joining path of directory 
+        const directoryPath = path.join(__dirname, i18nroot);
+
+        //passsing directoryPath and callback function
+        fs.readdir(directoryPath, function (err, files) {
+            //handling error
+            if (err) {
+                rejecting(err);
+                return console.log('Unable to scan directory: ' + err
+                    + "\n Might want to run from your root app dir"
+                );
+            }
+
+            resolving(files);
+        });
+    });
+}
