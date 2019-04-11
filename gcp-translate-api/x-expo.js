42b4ae3a47ed107f57483ff18bce10a28087cc91
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 07:47:04 -0400

    mastered npm i @exponent/json-file @stcgoal A reader/writter for JSON Files

--- gcp-translate-api/x-expo.js
+++ gcp-translate-api/x-expo.js
@@ -1,17 +1,24 @@
 var JsonFile = require('@exponent/json-file');
 //@exponent/json-file
+//@stcgoal Optimal management of JSON file 
+//@urir https://www.npmjs.com/package/@exponent/json-file
+
 
 var file = new JsonFile('package.json', {cantReadFileDefault: {}});
-
-//var obj = JSON.parse(file);
+ 
+//Read the File
  file.readAsync()
 .then((obj)=>
 {
 
-   
+   //@STCAction Show up each keys
    var keys = Object.keys(obj);
    for (var i = 0; i < keys.length; i++) {
       var v = obj[keys[i]];
       console.log(v);
    }
+})
+.catch((err)=>{
+   console.log(err);
 });
+
