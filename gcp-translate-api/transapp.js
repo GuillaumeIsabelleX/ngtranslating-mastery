6f51c25bca8b834dc2eabd23789712cf4f83c8b9
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 07:33:07 -0400

    reading all keys of each translation files

--- gcp-translate-api/transapp.js
+++ gcp-translate-api/transapp.js
@@ -21,7 +21,8 @@ readTransation()
         // var obj = JSON.parse(t);
          var keys = Object.keys(obj);
          for (var i = 0; i < keys.length; i++) {
-            console.log(obj[keys[i]]);
+            var v = obj[keys[i]];
+            console.log(v);
          }
       });
    })
